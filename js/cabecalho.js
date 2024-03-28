da84c1154e07bf9a1c258f0a8f1077cb1330937d
Giovanni Perrone <giovannipublicidade@gmail.com>
2024-03-28 13:42:54 -0300

    ajustei o design e criei o menu hambúrguer toggle

--- js/cabecalho.js
+++ js/cabecalho.js
@@ -1,4 +1,15 @@
+//exibindo e escondendo background do cabeçalho
 window.addEventListener("scroll", function() {
-    let header = document.querySelector('.cabecalho');
+    const header = document.querySelector('.cabecalho');
     header.classList.toggle('cabecalho-bg', window.scrollY > 0);
+});
+
+
+//menu hambúrguer
+const burger = document.querySelector('#burger');
+const menu = document.querySelector('.menu');
+
+burger.addEventListener("click", function() {
+    menu.classList.toggle('menu-ativado');
+    document.body.classList.toggle('bloquear-scroll');
 });
