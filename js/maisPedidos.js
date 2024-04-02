5287fceaea66638791b2ddfbadc3d2ce14acfb02
Giovanni Perrone <giovannipublicidade@gmail.com>
2024-04-02 15:31:04 -0300

    ajuste

--- js/maisPedidos.js
+++ js/maisPedidos.js
@@ -1,15 +1,11 @@
 //abrir e fechar modals
 const modalContainer = document.querySelector('.modal-container');
-let modal = document.querySelector('.modal');
-
 
 
 function abrirModal() {
     modalContainer.classList.add('modal-container-ativado');
-    // document.body.classList.add('bloquear-scroll');
 };
 
 function fecharModal() {
     modalContainer.classList.remove('modal-container-ativado');
-    // document.body.classList.remove('bloquear-scroll');
 };
