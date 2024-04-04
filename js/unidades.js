8aba29d8eb88c2801e5dae5b7b40702ad494222e
Giovanni Perrone <giovannipublicidade@gmail.com>
2024-04-04 11:35:09 -0300

    fechar dropdown ao clicar fora ou escolher um item
    section unidades

--- js/unidades.js
+++ js/unidades.js
@@ -2,6 +2,8 @@ const seta = document.querySelector('#seta-baixo');
 const btnUnidades = document.querySelector('#btn-unidades');
 const unidadesOpcoes = document.querySelector('.unidades-opcoes');
 
+let dropdownAberto = false;
+
 
 //declarando a função que troca a seta para cima e para baixo
 function trocaSeta() {
@@ -16,7 +18,17 @@ function trocaSeta() {
 //dropdown no escolha a unidade
 btnUnidades.addEventListener("click", function() {
     unidadesOpcoes.classList.toggle('unidades-opcoes-ativado');
+    dropdownAberto = true;
     trocaSeta();
+});
+
+
+window.addEventListener('click', function(evento) {
+    if (!btnUnidades.contains(evento.target) && dropdownAberto) {
+        unidadesOpcoes.classList.remove('unidades-opcoes-ativado');
+        dropdownAberto = false;
+        trocaSeta();
+    }
 });
 
 
