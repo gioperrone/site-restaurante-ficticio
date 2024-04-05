ae2548165d5005155b27592edf18e609cf78f6a0
Giovanni Perrone <giovannipublicidade@gmail.com>
2024-04-05 17:39:54 -0300

    fazendo media-queries
    Tablets e Desktops
    
    Falta ajustar o banner do site e tirar a rolagem dos cards na section mais-pedidos.

--- js/cabecalho.js
+++ js/cabecalho.js
@@ -1,5 +1,5 @@
 //rolagem do cabeçalho
-window.addEventListener("scroll", function() {
+window.addEventListener('scroll', function() {
     const header = document.querySelector('.cabecalho');
     header.classList.toggle('cabecalho-bg', window.scrollY > 0);
 });
@@ -9,7 +9,7 @@ window.addEventListener("scroll", function() {
 const burger = document.querySelector('#burger');
 const menu = document.querySelector('.menu');
 
-burger.addEventListener("click", function() {
+burger.addEventListener('click', function() {
     menu.classList.toggle('menu-ativado');
     document.body.classList.toggle('bloquear-scroll');
 });
@@ -21,3 +21,20 @@ function trocaBotao() {
         burger.innerHTML = "menu";
     };
 };
+
+
+//removendo bug de quando o menu hambúrguer está aberto e redimensiona a tela para a partir de 1200px
+function verificarLarguraTela() {
+    const larguraTela = window.innerWidth;
+    const menu = document.querySelector('.menu');
+    
+    if (larguraTela > 1200) {
+        menu.classList.remove('menu-ativado');
+        burger.innerHTML = "menu";
+        document.body.classList.remove('bloquear-scroll');
+    };
+};
+
+window.addEventListener('resize', verificarLarguraTela);
+
+verificarLarguraTela();
